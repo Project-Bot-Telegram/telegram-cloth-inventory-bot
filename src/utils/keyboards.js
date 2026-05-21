72aab7bbce559059674271598b05347156d422e9
PANHA00-cell <panha000000000@gmail.com>
2026-05-21 08:13:50 +0000

    feature-update-method-CRUD-in-category-model

--- src/utils/keyboards.js
+++ src/utils/keyboards.js
@@ -3,8 +3,14 @@ const { Markup } = require('telegraf');
 const mainMenuKeyboard = (isAdmin = false) => {
   const rows = [];
   if (isAdmin) {
-    rows.push(['Add Product', 'Add Category']);
+    rows.push(['Manage Product']);
+    rows.push(['Manage Category' , 'Manage Order']);
+    rows.push(['Support', 'Help']);
+    rows.push(['View Profile']);
+
+    return Markup.keyboard(rows).resize();
   }
+
   rows.push(['Show product', 'Orders History']);
   rows.push(['View Profile', 'Help']);
   rows.push(['Support']);
